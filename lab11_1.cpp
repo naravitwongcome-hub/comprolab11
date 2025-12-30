27213205e98f952ece512d72ac80e7476b1ab518
GAME <naravitwongcome@gmail.com>
2025-12-30 15:39:40 +0700

    lab11_1_complete.cpp

--- lab11_1.cpp
+++ lab11_1.cpp
@@ -1,2 +1,27 @@
 // cout << "Press Enter 3 times to reveal your future.";
 // cout << "You will get A in this 261102.";
+// cout << "Press Enter 3 times to reveal your future.";
+// cout << "You will get A in this 261102.";
+#include<iostream>
+#include<string>
+#include<ctime>
+#include<cstdlib>
+
+using namespace std;
+
+int main(){
+    srand(time(0));
+    cout << "Press Enter 3 times to reveal your future.";
+    
+    cin.get();
+    cin.get();
+    cin.get();
+    
+ string grade[]={ "A","B+","B","C+","C","D+","D","F","W"};
+ int lucky = rand()%9;
+ 
+    cout<<"You will get " << grade[lucky] << " in this 261102."<<endl;
+    
+    return 0;
+    
+}
